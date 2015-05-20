e1b5b46f0b669339a74acc6e8eb2087d6ac82b77
vijaygbarge <pramsvl_barge@yahoo.co.in>
2015-05-20 10:25:24 +0530

    a

---
+++ js/App.js
@@ -0,0 +1,37 @@
+﻿/* This Application is Designed and Developed by 
+ * Mirza Shareque shareque@outlook.com 
+ * Softmaque Consultant
+ * 20/05/2015
+ */
+
+//var url = 'http://192.168.140.99/ajax/index.html';
+var url = 'http://192.168.0.232/cgi-bin/login_page.tcl';
+$(function () {
+    $('#errorbox').hide();
+    BindDashboard();
+    var timer = setInterval(BindDashboard, 10000);
+});
+
+BindDashboard = function () {
+    $.get(url, function (data) {
+
+        $('#errorbox').hide();
+        innerD = $(data).find('#loginForm');
+        //Production
+        var TotalPro = innerD.find('.circleTotalProd .circleValue label').text().replace(/[^0-9\.]/gi, '');
+        var TodayPro = innerD.find('.circleTotalProd .circleDesc label').text().replace(/[^0-9\.]/gi, '');
+        //Co2 comsuption
+        var TotalCO2 = innerD.find('.circleCo2 .circleValue label').text().replace(/[^0-9\.]/gi, '');
+        var TotalBags = innerD.find('.circleCo2 .circleDesc label').text().replace(/[^0-9\.]/gi, '');
+        //Power output
+        var PowerOutput = innerD.find('.circlePwrOutput .circleValue label').text().replace(/[^0-9\.]/gi, '');
+
+        $('#lblTotalPro').text(TotalPro)
+        $('#lblTodayPro').text(TodayPro);
+        $('#lblTotalCO2').text(TotalCO2)
+        $('#lblTotalBags').text(TotalBags);
+        $('#lblPowerOutput').text(PowerOutput);
+    }).fail(function () {
+        $('#errorbox').show();
+    });
+}
