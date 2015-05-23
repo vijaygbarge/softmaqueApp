4e47e55ac031044a711bc8c54ee272373b497026
vijaygbarge <pramsvl_barge@yahoo.co.in>
2015-05-23 10:47:17 +0530

    s

--- js/App.js
+++ js/App.js
@@ -4,51 +4,54 @@
  * 20/05/2015
  */
 
-var url = 'http://192.168.140.99/ajax/index.html';
-//var url = 'http://192.168.0.232/cgi-bin/login_page.tcl';
+// url = 'http://192.168.140.99/ajax/index.html';
+var url = 'http://192.168.0.232/cgi-bin/login_page.tcl';
 $.support.cors = true;
 $(function () {
-    $('#errorbox').hide();
-    BindDashboard();
-    var timer = setInterval(BindDashboard, 10000);
+    //$('#errorbox').hide();
+    // BindDashboard();
+    //var timer = setInterval(BindDashboard, 10000);
 });
 
-BindDashboard = function () {
-    $.ajax({
-        url: url,
-        crossDomain: true,
-        success: function (data) {
-            innerD = $(data).find('#loginForm');
-            var TotalBags = innerD.find('.circleCo2 .circleDesc label').text().replace(/[^0-9\.]/gi, '');
-            $('#lblTotalBags').text(TotalBags);
-        },
-        error: function (xhr, status, error) {
-            if (xhr.responseText == '') {
-                xhr.responseText = 'No-Access-Allowed';
+BindDashboard = function (type) {
+    if (type == 1) {
+        $.ajax({
+            url: url,
+            crossDomain: true,
+            success: function (data) {
+                innerD = $(data).find('#loginForm');
+                var TotalBags = innerD.find('.circleCo2 .circleDesc label').text().replace(/[^0-9\.]/gi, '');
+                $('#lblTotalBags').text(TotalBags);
+            },
+            error: function (xhr, status, error) {
+                if (xhr.responseText == '') {
+                    xhr.responseText = 'No-Access-Allowed';
+                }
+                $('#lblTotalBags').text(xhr.responseText + ':' + status + ':' + error);
             }
-            $('#lblTotalBags').text(xhr.responseText + ':' + status + ':' + error);
-        }
-    });/*
-    $.get(url, function (data) {
+        });
+    }
 
-        $('#errorbox').hide();
-        innerD = $(data).find('#loginForm');
-        //Production
-        var TotalPro = innerD.find('.circleTotalProd .circleValue label').text().replace(/[^0-9\.]/gi, '');
-        var TodayPro = innerD.find('.circleTotalProd .circleDesc label').text().replace(/[^0-9\.]/gi, '');
-        //Co2 comsuption
-        var TotalCO2 = innerD.find('.circleCo2 .circleValue label').text().replace(/[^0-9\.]/gi, '');
-        var TotalBags = innerD.find('.circleCo2 .circleDesc label').text().replace(/[^0-9\.]/gi, '');
-        //Power output
-        var PowerOutput = innerD.find('.circlePwrOutput .circleValue label').text().replace(/[^0-9\.]/gi, '');
+    if (type == 2) {
+        $.get(url, function (data) {
+            $('#errorbox').hide();
+            innerD = $(data).find('#loginForm');
+            //Production
+            var TotalPro = innerD.find('.circleTotalProd .circleValue label').text().replace(/[^0-9\.]/gi, '');
+            var TodayPro = innerD.find('.circleTotalProd .circleDesc label').text().replace(/[^0-9\.]/gi, '');
+            //Co2 comsuption
+            var TotalCO2 = innerD.find('.circleCo2 .circleValue label').text().replace(/[^0-9\.]/gi, '');
+            var TotalBags = innerD.find('.circleCo2 .circleDesc label').text().replace(/[^0-9\.]/gi, '');
+            //Power output
+            var PowerOutput = innerD.find('.circlePwrOutput .circleValue label').text().replace(/[^0-9\.]/gi, '');
 
-        $('#lblTotalPro').text(TotalPro)
-        $('#lblTodayPro').text(TodayPro);
-        $('#lblTotalCO2').text(TotalCO2)
-        $('#lblTotalBags').text(TotalBags);
-        $('#lblPowerOutput').text(PowerOutput);
-    }).fail(function (erre) {
-        alert(erre);
-        $('#errorbox').show();
-*/
+            $('#lblTotalPro').text(TotalPro)
+            $('#lblTodayPro').text(TodayPro);
+            $('#lblTotalCO2').text(TotalCO2)
+            $('#lblTotalBags').text(TotalBags);
+            $('#lblPowerOutput').text(PowerOutput);
+        }).fail(function (erre) {
+            $('#lblTotalBags').text('Error:' + erre.responseText);
+        });
+    }
 }
